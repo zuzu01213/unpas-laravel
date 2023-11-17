ba73bc994cd34d9880a54f7a60f721a01141800c
azzza <139770919+zuzu01213@users.noreply.github.com>
2023-11-17 00:49:49 -0500

    menambah models post_

--- database/migrations/2023_11_17_054812_create_post__table.php
+++ database/migrations/2023_11_17_054812_create_post__table.php
@@ -8,27 +8,20 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     * 
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('post_', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
-     *
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('post_');
     }
 };
